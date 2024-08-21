2322bf5a4f3eca8d3dcf72b2c2a5f8f2bfabfd28
salvadorxe <salv.xe@gmail.com>
2024-08-21 18:50:52 -0400

    missing base in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://salvadorxe.github.io'
+  site: 'https://salvadorxe.github.io',
+  base: '/portfolio-1'
 });
