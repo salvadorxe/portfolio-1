39faf2b3f2ad503ac367589043e61b63b4856135
salvadorxe <salv.xe@gmail.com>
2024-08-21 22:31:40 -0400

    updated config for domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://salvadorxe.github.io',
-  base: '/portfolio-1'
+  site: 'https://salvadorantonio.com',
+  base: '/' // Changed from '/portfolio-1' to '/'
 });
